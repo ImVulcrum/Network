b5bfbd707c4fa53d1089699b7cea8142fb6e299a
wertzuz <quint.schartel@gmail.com>
2024-01-24 23:26:48 +0100

    1.000.000% speed up and smarter 'visited' handling by removing the bool

--- network/netzwerkimpl.go
+++ network/netzwerkimpl.go
@@ -14,7 +14,7 @@ type data struct {
 }
 
 type Knot struct {
-	Connectionn []*Connection
+	Connections []*Connection
 	inhalt      string
 	start_knot  bool
 }
@@ -28,7 +28,6 @@ type Item struct {
 	cKnot         *Knot
 	distance      int
 	previous_knot *Knot
-	visited       bool
 }
 
 // Priority Queue Implemenetation
@@ -91,21 +90,11 @@ func (pq PriorityQueue) GetIndexOfElementByKnot(knot *Knot) int {
 func (pq PriorityQueue) Print() {
 	for i := 0; i < len(pq); i++ {
 		if pq[i].previous_knot == nil {
-			fmt.Println(pq[i].cKnot.inhalt, pq[i].distance, "Keiner", pq[i].visited)
+			fmt.Println(pq[i].cKnot.inhalt, pq[i].distance, "nil")
 		} else {
-			fmt.Println(pq[i].cKnot.inhalt, pq[i].distance, pq[i].previous_knot.inhalt, pq[i].visited)
+			fmt.Println(pq[i].cKnot.inhalt, pq[i].distance, pq[i].previous_knot.inhalt)
 		}
 	}
-}
-
-func (pq PriorityQueue) GetIndexOfNearestNonVisitedItem() int {
-	for i := 0; i < len(pq); i++ {
-		if !pq[i].visited {
-			return i
-		}
-	}
-
-	return -1
 }
 
 func (pq *PriorityQueue) AddElement(item Item) {
@@ -185,19 +174,19 @@ func (n *data) Print(tag string) {
 }
 
 func (n *data) MoveByContent(content string) error {
-	if len(n.current_knot.Connectionn) == 0 {
+	if len(n.current_knot.Connections) == 0 {
 		return fmt.Errorf("the current knot has no connections")
 	}
 
 	var i int
-	for i = 0; i < len(n.current_knot.Connectionn); i++ {
-		if n.current_knot.Connectionn[i].ziel.inhalt == content {
-			n.current_knot = n.current_knot.Connectionn[i].ziel
+	for i = 0; i < len(n.current_knot.Connections); i++ {
+		if n.current_knot.Connections[i].ziel.inhalt == content {
+			n.current_knot = n.current_knot.Connections[i].ziel
 			return nil
 		}
 	}
 
-	if i == len(n.current_knot.Connectionn) {
+	if i == len(n.current_knot.Connections) {
 		return fmt.Errorf("the current knot has no connection to the given knot")
 	}
 	return nil
@@ -207,41 +196,41 @@ func (n *data) MoveByWeight(type_of_movement int) error {
 	//-1 smallest
 	//-2 highest
 
-	if len(n.current_knot.Connectionn) == 0 {
+	if len(n.current_knot.Connections) == 0 {
 		return fmt.Errorf("the current knot has no connections")
 	}
 	var index int = 0
 
 	if type_of_movement >= 0 {
 		var i int
-		for i = 0; i < len(n.current_knot.Connectionn); i++ {
-			if n.current_knot.Connectionn[i].gewicht == type_of_movement {
-				n.current_knot = n.current_knot.Connectionn[i].ziel
+		for i = 0; i < len(n.current_knot.Connections); i++ {
+			if n.current_knot.Connections[i].gewicht == type_of_movement {
+				n.current_knot = n.current_knot.Connections[i].ziel
 				return nil
 			}
 		}
-		if i == len(n.current_knot.Connectionn) {
+		if i == len(n.current_knot.Connections) {
 			return fmt.Errorf("the current knot has no connection with he given weight")
 		}
 	} else if type_of_movement == -1 {
-		var current_smallest int = n.current_knot.Connectionn[0].gewicht
+		var current_smallest int = n.current_knot.Connections[0].gewicht
 
-		for i := 0; i < len(n.current_knot.Connectionn); i++ {
-			if n.current_knot.Connectionn[i].gewicht < current_smallest {
-				current_smallest = n.current_knot.Connectionn[i].gewicht
+		for i := 0; i < len(n.current_knot.Connections); i++ {
+			if n.current_knot.Connections[i].gewicht < current_smallest {
+				current_smallest = n.current_knot.Connections[i].gewicht
 				index = i
 			}
 		}
 	} else if type_of_movement == -2 {
-		var current_highest int = n.current_knot.Connectionn[0].gewicht
-		for i := 0; i < len(n.current_knot.Connectionn); i++ {
-			if n.current_knot.Connectionn[i].gewicht > current_highest {
-				current_highest = n.current_knot.Connectionn[i].gewicht
+		var current_highest int = n.current_knot.Connections[0].gewicht
+		for i := 0; i < len(n.current_knot.Connections); i++ {
+			if n.current_knot.Connections[i].gewicht > current_highest {
+				current_highest = n.current_knot.Connections[i].gewicht
 				index = i
 			}
 		}
 	}
-	n.current_knot = n.current_knot.Connectionn[index].ziel
+	n.current_knot = n.current_knot.Connections[index].ziel
 	return nil
 }
 
@@ -299,7 +288,7 @@ func (n *data) AddKnot(inhalt string, outgoing_con []*Connection, incoming_con [
 	}
 
 	for i := 0; i < len(outgoing_con); i++ {
-		knot.Connectionn = append(knot.Connectionn, outgoing_con[i])
+		knot.Connections = append(knot.Connections, outgoing_con[i])
 		if outgoing_con[i].ziel.start_knot { //if one connection goes to a start knot this flag must be removed, cuz is is't a start knot anymore
 			outgoing_con[i].ziel.start_knot = false
 		}
@@ -309,7 +298,7 @@ func (n *data) AddKnot(inhalt string, outgoing_con []*Connection, incoming_con [
 		var connection *Connection = new(Connection)
 		connection.gewicht = incoming_con[i].gewicht
 		connection.ziel = knot
-		incoming_con[i].ziel.Connectionn = append(incoming_con[i].ziel.Connectionn, connection)
+		incoming_con[i].ziel.Connections = append(incoming_con[i].ziel.Connections, connection)
 	}
 	n.knots = append(n.knots, knot)
 
@@ -321,10 +310,10 @@ func (n *data) AddKnot(inhalt string, outgoing_con []*Connection, incoming_con [
 func (knot *Knot) String() string {
 	var erg string
 	erg = erg + fmt.Sprintln(knot.inhalt)
-	for i := 0; i < len(knot.Connectionn); i++ {
+	for i := 0; i < len(knot.Connections); i++ {
 		erg = erg + "   --"
-		erg = erg + fmt.Sprint(knot.Connectionn[i].gewicht, "-->")
-		erg = erg + fmt.Sprintln(knot.Connectionn[i].ziel.inhalt)
+		erg = erg + fmt.Sprint(knot.Connections[i].gewicht, "-->")
+		erg = erg + fmt.Sprintln(knot.Connections[i].ziel.inhalt)
 	}
 	return erg
 }
@@ -332,55 +321,49 @@ func (knot *Knot) String() string {
 func (knot *Knot) DeleteConnectionByDestination(delition_destination *Knot) {
 	var i int
 
-	for i = 0; i < len(knot.Connectionn); i++ {
-		if knot.Connectionn[i].ziel == delition_destination {
+	for i = 0; i < len(knot.Connections); i++ {
+		if knot.Connections[i].ziel == delition_destination {
 			break
 		}
 	}
 
 	//delete function
-	if i < len(knot.Connectionn) {
-		knot.Connectionn[i] = knot.Connectionn[len(knot.Connectionn)-1]
-		knot.Connectionn[len(knot.Connectionn)-1] = nil
-		knot.Connectionn = knot.Connectionn[:len(knot.Connectionn)-1]
+	if i < len(knot.Connections) {
+		knot.Connections[i] = knot.Connections[len(knot.Connections)-1]
+		knot.Connections[len(knot.Connections)-1] = nil
+		knot.Connections = knot.Connections[:len(knot.Connections)-1]
 	}
 }
 
 func (n *data) Dijkstras(startknot *Knot) PriorityQueue {
-	n.SetCurrentKnot(startknot)
 
 	table := make(PriorityQueue, 0)
 
 	for i := 0; i < len(n.knots); i++ {
 		if n.knots[i] == startknot {
-			table.AddElement(Item{cKnot: startknot, distance: 0, previous_knot: nil, visited: true})
+			table.AddElement(Item{cKnot: startknot, distance: 0, previous_knot: nil})
 		} else {
-			table.AddElement(Item{cKnot: n.knots[i], distance: -3, previous_knot: nil, visited: false})
+			table.AddElement(Item{cKnot: n.knots[i], distance: -3, previous_knot: nil})
 		}
 	}
 
-	for {
-		connections := n.current_knot.Connectionn
+	for index := 0; index < len(table); index++ {
+		n.SetCurrentKnot(table[index].cKnot)
+		connections := n.current_knot.Connections
+
 		for i := 0; i < len(connections); i++ {
+			distance := table[table.GetIndexOfElementByKnot(n.GiveCurrentKnot())].distance
 			//überprüfen ob kanten gewicht + gewicht von wo wir sind kleiner als das was schon in der tabelle steht
 
-			current_distant_to_knot := table[table.GetIndexOfElementByKnot(n.GiveCurrentKnot())].distance + connections[i].gewicht
+			ziel := table.GetIndexOfElementByKnot(connections[i].ziel)
+			current_distant_to_knot := distance + connections[i].gewicht
 
-			if current_distant_to_knot < table[table.GetIndexOfElementByKnot(connections[i].ziel)].distance || table[table.GetIndexOfElementByKnot(connections[i].ziel)].distance == -3 {
-				table.ChangeElement(table.GetIndexOfElementByKnot(connections[i].ziel), current_distant_to_knot)
-				table[table.GetIndexOfElementByKnot(connections[i].ziel)].previous_knot = n.GiveCurrentKnot()
+			if table[ziel].distance == -3 || current_distant_to_knot < table[ziel].distance {
+				table[ziel].previous_knot = n.GiveCurrentKnot()
+				table.ChangeElement(ziel, current_distant_to_knot)
+
 			}
 		}
-
-		index_of_nearest_item := table.GetIndexOfNearestNonVisitedItem()
-
-		if index_of_nearest_item == -1 {
-			break
-		}
-
-		table[index_of_nearest_item].visited = true
-		//table[index_of_nearest_item].previous_knot = n.GiveCurrentKnot()
-		n.SetCurrentKnot(table[index_of_nearest_item].cKnot)
 	}
 
 	return table
